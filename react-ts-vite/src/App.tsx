d5d5bd8574a4e47a60d2407e30eb3ed68a87c382
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-10-18 10:42:15 +0800

    update config

--- react-ts-vite/src/App.tsx
+++ react-ts-vite/src/App.tsx
@@ -1,21 +1,8 @@
-import { useState } from 'react'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p className="text-lg bg-slate-600">
+        tailwindcss
       </p>
     </div>
   )
